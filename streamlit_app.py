e2856a4179c2c05cb2b81f2b16bec2e2764712a5
clenio77 <mouraaf@hotmail.com>
2022-02-12 17:42:28 -0300

    update css

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,9 @@ import qrcode
 from PIL import Image
 
 
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
 qr = qrcode.QRCode(version=1,
                    error_correction=qrcode.constants.ERROR_CORRECT_L,
                    box_size=10, border=14)
@@ -36,14 +39,14 @@ def main():
     choice = st.sidebar.selectbox('Menu',menu)
 
 
-    st.title('Generate e Decoder QR_Code')
+    st.title('Gerador de QR_Code')
     if choice == 'Home':
-        st.subheader('Home')
+        #st.subheader('Home')
 
         # entrada de dados
         with st.form(key='myqr_form'):
-            raw_text = st.text_input('Escreva aqui')
-            name_image = st.text_input('Escreva o nome da sua imagem...')
+            raw_text = st.text_input('Escreva ou cole o endereço do conteúdo.... ')
+            name_image = st.text_input('Escreva o nome  para salvar sua imagem...')
             submit_button = st.form_submit_button('Gerar QR_Code')
         if submit_button:
             col1,col2 = st.columns(2)
@@ -66,32 +69,35 @@ def main():
                 st.write(raw_text)
     elif choice == 'Decoder':
         st.subheader('Decoder')
-
-        image_file = st.file_uploader('Upload Image', type=['jpg','png','jpeg'])
-        if image_file is not None:
-            # Method 1: Display Image
-            img = load_image(image_file)
-            st.image(img)
-
-            # Method 2: Using opencv
-            # file_bytes = np.asanyarray(bytearray(image_file.read()),dtype=np.uint8)
-            # opencv_image = cv2.imdecode(file_bytes,1)
-            
-            # c1,c2 = st.columns(2)
-            # with c1:
-            #     st.image(opencv_image)
-            #
-            # with c2:
-            #     st.info('QR Code Decodificado')
-            #     det = cv2.QRCodeDetector()
-            #     retval,points,straight_qrcode = det.detectAndDecode(opencv_image)
-            #
-            #     # Retval is for the text
-            #     st.write(retval)
-            
-            #st.image(opencv_image)
+        st.write('Em construção')
+        
+    #
+    #     image_file = st.file_uploader('Upload Image', type=['jpg','png','jpeg'])
+    #     if image_file is not None:
+    #         # Method 1: Display Image
+    #         img = load_image(image_file)
+    #         #st.image(img)
+    #
+    #         # Method 2: Using opencv
+    #         # file_bytes = np.asanyarray(bytearray(image_file.read()),dtype=np.uint8)
+    #         # opencv_image = cv2.imdecode(file_bytes,1)
+    #
+    #         c1,c2 = st.columns(2)
+    #         with c1:
+    #              st.image(img)
+    #         #
+    #         with c2:
+    #              st.info('QR Code Decodificado')
+    #         #     det = cv2.QRCodeDetector()
+    #         #     retval,points,straight_qrcode = det.detectAndDecode(opencv_image)
+    #         #
+    #         #     # Retval is for the text
+    #              st.write(raw_text)
+    #
+    #         #st.image(opencv_image)
     else:
         st.subheader('Sobre')
+        st.write('Em construção')
 
 
 if __name__ == '__main__':
